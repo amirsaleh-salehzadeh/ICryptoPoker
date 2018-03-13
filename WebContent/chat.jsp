70a2884d7925e1be37f4c4764cd9aecb06d8eee4
maninjwa <maninjwa@github.com>
2018-03-13 12:37:39 +0200

    commit

---
+++ WebContent/chat.jsp
@@ -0,0 +1,54 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<script src="js/jquery.min.js"></script>
+
+<script type="text/javascript">
+
+var websocket;
+function start() {
+    var uri = "ws://localhost:8080/ICryptoPoker/chat/"+$("#sID").val(); // ws -> not secure, wss (ssl)
+    websocket = new WebSocket(uri);
+    // override Methode: when recive msg from server.
+    websocket.onmessage = function (msg) {
+      console.log("msg recived from server: " + msg.data);
+    }
+    // override Methode: when server throw an error.
+    websocket.onerror = function (evt) {
+      console.log("error from server: Can't establish a connection to the server!");
+      console.info(evt);
+    }
+    // override Methode: when server close the connection.
+    websocket.onclose = function () {
+      console.log("Server close connection.");
+	  
+    }
+    // override Methode: when websocket connection was established
+    websocket.onopen = function () {
+      // send a msg to server
+      websocket.send("getID");
+      $("#sID").val(websocket.toString());
+    }
+    
+  }
+  function sendMessage(){
+	  websocket.send($("#msg").val()) ;//this wasnt a genreal variable firstly
+  }
+</script>
+
+</head>
+<body>
+  <textarea  id= "msg" rows="10" cols="45"></textarea>
+ 
+  <br/>
+ TO <input type ="text" id = "rID" size ="50"/> <br/>
+ FROM <input type ="text" id = "sID" size ="50"/> <br/>
+  
+  <input type= "button" value ="send" onclick="sendMessage()"/>
+  <input type= "button" value ="start" onclick="start()"/>
+</body>
+</html>
--- src/services/websockets/TableWebsocket.java
+++ src/services/websockets/TableWebsocket.java
@@ -4,12 +4,12 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
-import javax.
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonNode;
@@ -20,57 +20,54 @@ import org.codehaus.jettison.json.JSONObject;
 
 
 
-@ServerEndpoint("/TableWebsocket")
+@ServerEndpoint("/chat/{uid}")
 public class TableWebsocket {
-	static Set<Session> users = Collections.synchronizedSet(new HashSet<Session>()) ;
-	
-	
-	@OnOpen
-	public void handleOpen(Session user){
-		
-		users.add(user) ;
-		System.out.println("player has joined");
-	}
-	
-	@OnMessage
-	public void handleMessage(String message , Session userSession){
-		String username = (String)userSession.getUserProperties().get("username") ;
-		if(username==null){
-			userSession.getUserProperties().put("username",message) ;
-			userSession.getBasicRemote().sendText(buildJsonData("System", message)) ;
-		}else{
-			Iterator<Session> iterator = users.iterator() ;
-			while(iterator.hasNext()){
-				iterator.next().getBasicRemote().sendText(buildJsonData(username,message)) ;
-			}
-		}
-		
-	}
-	
-	@OnClose
-	public void handleClose(Session user){
-		users.remove(user);
-		System.out.println("player has lefted");
-	}
-	
-	@OnError
-	public void handleError(Throwable t){
-		
-		t.printStackTrace() ;
-		
-	}
-	
-	private String buildJsonData(String username,String message){
-		 
-		
-		ObjectMapper mapper = new ObjectMapper() ;
-		
-		String[] temp = {"message",username +":" +message};
-		 String json = mapper.writeValueAsString(temp) ;
-		
-		return json ;
-		
-	}
-	
+	private static Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
 
+	  @OnOpen
+	  public void onOpen(Session session) {
+	    sessions.add(session);
+	    System.out.println("on open");
+	  }
+
+	  @OnClose
+	  public void onClose(Session session) {
+	    sessions.remove(session);
+	    System.out.println("on close");
+	  }
+
+	  @OnMessage
+	  public void onReciveMsg(Session clientSession, String msg, @PathParam("uid") String uid) throws InterruptedException {
+	    System.out.println("on recive msg." +msg);
+	    for (Session session : sessions) {
+	      session.getAsyncRemote().sendText("msg for all clients: " + msg);
+	    }
+
+    Thread.sleep(5000);
+	    clientSession.getAsyncRemote().sendText("your personal message from server");
+//    Thread.sleep(5000);
+//	    clientSession.getAsyncRemote().sendText("your msg was: " + msg);
+//	  }
+	
+//	private String buildJsonData(String username,String message){
+//		 String json = "" ;
+//		 JSONObject temp = null ;
+//		try{
+//		ObjectMapper mapper = new ObjectMapper() ;
+//		
+//	    temp  = new JSONObject() ;
+//		temp.put("mesage", username +":" +message) ;
+//		Json as = new Json() ;
+//		//String[] temp = {"message",};
+//		  //json = mapper.writeValueAsString(temp) ;
+//		}catch(Exception e){
+//			e.printStackTrace() ;
+//		}
+//		
+//		return String.valueOf(temp) ;
+//		
+//		
+//	}
+	
+	  }
 }

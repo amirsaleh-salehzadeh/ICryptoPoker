9ab8559ba647e667a8875924c2dc9275f8332f2c
NeilB <s217477577@nmmu.ac.za>
2018-03-14 22:44:26 +0200

    card css

--- WebContent/js/game/table.js
+++ WebContent/js/game/table.js
@@ -23,20 +23,18 @@ $(window)
 										$(this).html(content);
 									});
 					fitElementsWithinScreen();
-					startTheGame();
+					startAHand();
 				});
 function addAPlyerToTable(playerName, chips) {
-	var content = "<div class='sitPlaceThumbnail'>"
-			+ "<img alt='' src='images/game/user.png' height='100%' /></div>"
-			+ "<div class='playerNamePlace'> "
+	var content =  "<div class='ui-grid-a'><div class='ui-block-a playerBlock1'><div class='playerNamePlace'> "
 			+ playerName
 			+ "</div><div class='playerTotalChipsPlace'>$"
 			+ chips
-			+ "</div>"
-			+ "<div class='ui-grid-a playerCardsContainer' id='cards"
+			+ "</div><div class='sbchipcontainer'></div></div>"
+			+ "<div class='ui-block-b'><div class='playerCardsContainer' id='cards"
 			+ playerName
-			+ "'><div class='ui-block-a card1'>a</div><div class='ui-block-b card2'>b</div>"
-			+ "</div><div class='sbchipcontainer'></div>";
+			+ "'><div class=' card1 card-holder'>a</div><div class='card2 card-holder'>b</div>"
+			+ "</div></div></div>";
 	return content;
 }
 
@@ -44,20 +42,13 @@ function fitElementsWithinScreen() {
 //	var mainBoardHeight = $(window).height() - 88;// Bottom and
 //	$("#mainTable").css("height", $("mainTableParentDIV").height());
 //	$("#mainTable").css("width", $("mainTableParentDIV").width());
-	$(".sitPlaceThumbnail").each(function() {
-		$(this).width($("#userSitPlace").height() / 2);
-		$(this).height($("#userSitPlace").height() / 2);
-		$(this).trigger("create");
-	});
-	$(".sitPlaceThumbnailEmpty").each(function() {
-		$(this).width($("#userSitPlace").height() / 2);
-		$(this).height($("#userSitPlace").height() / 2);
-		$(this).trigger("create");
-	});
+	
 	$(".playerCardsContainer").each(function() {
 		$(this).width($(this).parent().parent().width() / 2);
 		$(this).height($(this).parent().parent().height() / 2);
+		($(this).find(".card2")).find(".card-img").css("left",$(this).find(".card2").css("left"));
 		$(this).trigger("create");
+		
 	});
 	// $("#mainBoardContainerDIV").width();
 }
@@ -101,7 +92,7 @@ function generateACard(cardVal, divID, cardNumber) {
 			+ text + "</span><span class='card-img " + color + "'>" + img
 			+ "</span></div>";
 	if (divID=="flopsContainer"){
-	$("#flop" + cardNumber).html(res).trigger("create");// i only show this for ids starting with flop u shud make a small change here...
+	$("#flop" + cardNumber).html(res).trigger("create");
 	}else{
 		$("#" + divID).find(".card"+ cardNumber).html(res).trigger("create");
 		
